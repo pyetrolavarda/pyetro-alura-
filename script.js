1e07c99086e07138a5102ee06813f1bc7a0b1e11
pyetrolavarda <pyetro.lavarda@escola.pr.gov.br>
2026-08-04 14:19:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-
 avanca.forEach(button => {
-   button.addEventListener('click', function(){
-       const atual = document.querySelector('.ativo');
-       const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
-
-
-       atual.classList.remove('ativo');
-       document.getElementById(proximoPasso).classList.add('ativo');
-   })
-})
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPassoId = 'passo-' + this.getAttribute('data-proximo');
+        
+        atual.classList.remove('ativo');
+        
+        const proximoElemento = document.getElementById(proximoPassoId);
+        if (proximoElemento) {
+            proximoElemento.classList.add('ativo');
+        }
+    });
+});
